258937bc575bec23acbe4cacef3ef13b9eb8f65b
naumovichhh <32872842+naumovichhh@users.noreply.github.com>
2017-11-01 12:57:23 +0200

    Add files via upload

---
+++ QuickSort/QuickSort/MergeSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class MergeSort
+    {
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Sort(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int indexLowest, int indexHighest)
+        {
+            if (indexHighest <= indexLowest)
+                return;
+            int mid = indexLowest + (indexHighest - indexLowest) / 2;
+            Sort(array, indexLowest, mid);
+            Sort(array, mid + 1, indexHighest);
+            int[] buf = new int[array.Length];
+            Array.Copy(array, buf, array.Length);
+            for (int k = indexLowest; k <= indexHighest; k++)
+            {
+                buf[k] = array[k];
+            }
+            int i = indexLowest, j = mid + 1;
+            for (int k = indexLowest; k <= indexHighest; k++)
+            {
+                if (i > mid)
+                {
+                    array[k] = buf[j];
+                    j++;
+                }
+                else if (j > indexHighest)
+                {
+                    array[k] = buf[i];
+                    i++;
+                }
+                else if (buf[j] < buf[i])
+                {
+                    array[k] = buf[j];
+                    j++;
+                }
+                else
+                {
+                    array[k] = buf[i];
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSort/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public static class QuickSort
+    {
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Sort(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int indexLowest, int indexHighest)
+        {
+            int i = indexLowest, j = indexHighest;
+            int x = array[(i + j) / 2];
+            do
+            {
+                while (array[i] < x)
+                {
+                    ++i;
+                }
+                while (array[j] > x)
+                {
+                    --j;
+                }
+                if (i <= j)
+                {
+                    Swap(ref array[i], ref array[j]);
+                    ++i;
+                    --j;
+                }
+            } while (i <= j);
+            if (indexLowest < j)
+            {
+                Sort(array, indexLowest, j);
+            }
+            if (i < indexHighest)
+            {
+                Sort(array, i, indexHighest);
+            }
+        }
+    }
+}
